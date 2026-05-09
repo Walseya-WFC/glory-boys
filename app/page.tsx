9fb02a634b58cdc2980baf920360d37031dd82de
Glory Boys Guild <walseyafunchannel@gmail.com>
2026-05-09 23:32:55 +0200

    clean version again nofog etc

--- app/page.tsx
+++ app/page.tsx
@@ -120,9 +120,6 @@ export default function GloryBoysGuildSite() {
       {/* DARK SHADE */}
       <div className="absolute inset-0 bg-black/25" />
 
-      <div className="ash-layer" />
-      <div className="lightning-layer" />
-
       <div className="relative z-10">
 
         {/* HERO */}
